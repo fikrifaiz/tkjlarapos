bca46f1ff46703b3948f482980fd5ff31b94b695
fikrifaiz <faizfikri88@gmail.com>
2018-04-12 16:28:35 +0700

    tkjlarapos

---
+++ resources/views/category/index.blade.php
@@ -0,0 +1,12 @@
+@extends('layout');
+@section('content');
+
+	<table class="table table-bordered">
+		<tr>
+			<th>No</th>
+			<th>Category</th>
+			<th>Action</th>
+		</tr>
+	</table>
+
+@endsection
